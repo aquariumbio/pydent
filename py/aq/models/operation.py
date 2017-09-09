6d8efbf747559956be791bf1df473d4f17ac5e1d
Eric Klavins <klavins@uw.edu>
2017-09-09 08:20:55 -0700

    data associations for operations and plans

--- py/aq/models/operation.py
+++ py/aq/models/operation.py
@@ -3,6 +3,7 @@ import aq
 class OperationRecord(aq.Record):
     def __init__(self,model,data):
         super(OperationRecord,self).__init__(model,data)
+        self.has_many_generic("data_associations", aq.DataAssociation)
 
 class OperationModel(aq.Base):
 
--- py/aq/models/plan.py
+++ py/aq/models/plan.py
@@ -3,6 +3,7 @@ import aq
 class PlanRecord(aq.Record):
     def __init__(self,model,data):
         super(PlanRecord,self).__init__(model,data)
+        self.has_many_generic("data_associations", aq.DataAssociation)
 
 class PlanModel(aq.Base):
 
