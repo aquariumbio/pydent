82a1d72d890404326b00d9b222f65c330f6acf06
Eric Klavins <klavins@uw.edu>
2017-09-09 08:19:29 -0700

    data associations and object type for item

--- py/aq/models/data_association.py
+++ py/aq/models/data_association.py
@@ -1,8 +1,18 @@
 import aq
+import json
 
 class DataAssociationRecord(aq.Record):
     def __init__(self,model,data):
         super(DataAssociationRecord,self).__init__(model,data)
+        self.has_one("upload", aq.Upload)
+
+    @property
+    def value(self):
+        obj = json.loads(self.object)
+        if self.key in obj:
+            return obj[self.key]
+        else:
+            return None
 
 class DataAssociationModel(aq.Base):
 
--- py/aq/record.py
+++ py/aq/record.py
@@ -8,6 +8,7 @@ class Record:
         self.__data = data
         self.__has_one = {}
         self.__has_many = {}
+        self.__has_many_generic = {}
         for key in data:
             setattr(self,key,data[key])
 
@@ -19,13 +20,13 @@ class Record:
 
     def get_one(self,name):
         reference = name + "_id"
-        result = self.__has_one[name]["model"].find(getattr(self,reference))
+        fid = getattr(self,reference)
+        if fid:
+            result = self.__has_one[name]["model"].find(fid)
+        else:
+            result = None
         setattr(self,name,result)
         return result
-
-    def snake(self,name):
-        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
     def has_many(self,name,model,opts={}):
         self.__has_many[name] = { "model": model }
@@ -39,6 +40,19 @@ class Record:
         results = self.__has_many[name]["model"].where({reference: self.id})
         return results
 
+    def has_many_generic(self,name,model):
+        self.__has_many_generic[name] = { "model": model }
+        if name in self.__data:
+            records = [ model.record(r) for r in self.__data[name] ]
+            setattr(self,name,records)
+
+    def get_many_generic(self,name):
+        results = self.__has_many_generic[name]["model"].where({
+            "parent_class": self.model.name,
+            "parent_id": self.id
+        })
+        return results
+
     def __getattr__(self, name):
 
         # print("method missing for " + name)
@@ -49,9 +63,18 @@ class Record:
         def __get_many_wrapper(name):
             return self.get_many(name)
 
+        def __get_many_generic_wrapper(name):
+            return self.get_many_generic(name)
+
         if name in self.__has_one:
             return __get_one_wrapper(name)
         elif name in self.__has_many:
             return __get_many_wrapper(name)
+        elif name in self.__has_many_generic:
+            return __get_many_generic_wrapper(name)
         else:
             raise Exception("Association " + name + " not found.")
+
+    def snake(self,name):
+        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
