6b095704c5e701c21fd858ba9b1233442536f17d
Eric Klavins <klavins@uw.edu>
2017-09-08 19:37:16 -0700

    download data files from s3

--- py/aq/models/upload.py
+++ py/aq/models/upload.py
@@ -1,8 +1,20 @@
 import aq
+import requests
 
 class UploadRecord(aq.Record):
+
     def __init__(self,model,data):
         super(UploadRecord,self).__init__(model,data)
+
+    @property
+    def temp_url(self):
+        """Returns a temporary S3 url that is valid for a few seconds"""
+        return aq.Upload.where({"id": self.id},{"methods": ["url"]})[0].url
+
+    @property
+    def data(self):
+        r = requests.get(self.temp_url)
+        return r.content
 
 class UploadModel(aq.Base):
 
