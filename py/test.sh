ca3ed013af2ac735ed1ca8c2d610938f65c83c8d
Eric Klavins <klavins@uw.edu>
2017-10-13 12:52:21 -0700

    easier to look at test output

--- py/test.sh
+++ py/test.sh
@@ -2,10 +2,10 @@
 
 for test in test/*.py
 do
-  printf $test
-  if python $test; then
-    printf "\e[32m ... ok\n\e[39m"
+  printf "%-50s %s" $test
+  if python $test > /dev/null 2> /dev/null; then
+    printf "    \e[32m ... ok\n\e[39m"
   else
-    printf "\e[31m ... error\n\e[39m"
+    printf "    \e[31m ... error\n\e[39m"
   fi
 done
