78421c4196107387c340d5d104da4565f1bae7ca
Eric Klavins <klavins@uw.edu>
2017-10-13 15:51:17 -0700

    override to_json example

--- py/test/serialize.py
+++ py/test/serialize.py
@@ -17,3 +17,22 @@ print(json.dumps(p.to_json(include=[
         ]
     }
 ])))
+
+# temporarily override the to_json method of field types
+def my_to_json(self,include=[],exclude=[]):
+    return { "i": len(include), "e": len(exclude) }
+
+aq.FieldTypeRecord.to_json = my_to_json
+
+print(json.dumps(p.to_json(include=[
+    {
+        "field_types": [
+            {
+                "allowable_field_types": [
+                    "object_type",
+                    "sample_type"
+                ]
+            }
+        ]
+    }
+])))
