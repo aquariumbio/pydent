d2a4c501e388eac2df54fe8d3af896c115a4a9a6
jvrana <justin.vrana@gmail.com>
2017-10-23 14:56:19 -0700

    prepare for models replacement

--- pydent/aq/__init__.py
+++ pydent/aq/__init__.py
@@ -2,32 +2,32 @@ from .base     import Base
 from .record   import Record, RecordHook
 from .utils import utils
 from .session import Session
-
+from .pillow_models import *
 from pydent.aq.algorithms import *
 
-from pydent.aq.models.account                  import Account, AccountRecord
-from pydent.aq.models.allowable_field_type     import AllowableFieldType, AllowableFieldTypeRecord
-from pydent.aq.models.budget                   import Budget, BudgetRecord
-from pydent.aq.models.aqcode                     import Code, CodeRecord
-from pydent.aq.models.collection               import Collection, CollectionRecord
-from pydent.aq.models.data_association         import DataAssociation, DataAssociationRecord
-from pydent.aq.models.field_type               import FieldType, FieldTypeRecord
-from pydent.aq.models.field_value              import FieldValue, FieldValueRecord
-from pydent.aq.models.group                    import Group, GroupRecord
-from pydent.aq.models.invoice                  import Invoice, InvoiceRecord
-from pydent.aq.models.item                     import Item, ItemRecord
-from pydent.aq.models.job                      import Job, JobRecord
-from pydent.aq.models.job_association          import JobAssociation, JobAssociationRecord
-from pydent.aq.models.library                  import Library, LibraryRecord
-from pydent.aq.models.membership               import Membership, MembershipRecord
-from pydent.aq.models.object_type              import ObjectType, ObjectTypeRecord
-from pydent.aq.models.operation_type           import OperationType, OperationTypeRecord
-from pydent.aq.models.operation                import Operation, OperationRecord
-from pydent.aq.models.plan_association         import PlanAssociation, PlanAssociationRecord
-from pydent.aq.models.plan                     import Plan, PlanRecord
-from pydent.aq.models.sample_type              import SampleType, SampleTypeRecord
-from pydent.aq.models.sample                   import Sample, SampleRecord
-from pydent.aq.models.upload                   import Upload, UploadRecord
-from pydent.aq.models.user_budget_association  import UserBudgetAssociation, UserBudgetAssociationRecord
-from pydent.aq.models.user                     import User, UserRecord
-from pydent.aq.models.wire                     import Wire, WireRecord
+# from pydent.aq.models.account                  import Account, AccountRecord
+# from pydent.aq.models.allowable_field_type     import AllowableFieldType, AllowableFieldTypeRecord
+# from pydent.aq.models.budget                   import Budget, BudgetRecord
+# from pydent.aq.models.aqcode                     import Code, CodeRecord
+# from pydent.aq.models.collection               import Collection, CollectionRecord
+# from pydent.aq.models.data_association         import DataAssociation, DataAssociationRecord
+# from pydent.aq.models.field_type               import FieldType, FieldTypeRecord
+# from pydent.aq.models.field_value              import FieldValue, FieldValueRecord
+# from pydent.aq.models.group                    import Group, GroupRecord
+# from pydent.aq.models.invoice                  import Invoice, InvoiceRecord
+# from pydent.aq.models.item                     import Item, ItemRecord
+# from pydent.aq.models.job                      import Job, JobRecord
+# from pydent.aq.models.job_association          import JobAssociation, JobAssociationRecord
+# from pydent.aq.models.library                  import Library, LibraryRecord
+# from pydent.aq.models.membership               import Membership, MembershipRecord
+# from pydent.aq.models.object_type              import ObjectType, ObjectTypeRecord
+# from pydent.aq.models.operation_type           import OperationType, OperationTypeRecord
+# from pydent.aq.models.operation                import Operation, OperationRecord
+# from pydent.aq.models.plan_association         import PlanAssociation, PlanAssociationRecord
+# from pydent.aq.models.plan                     import Plan, PlanRecord
+# from pydent.aq.models.sample_type              import SampleType, SampleTypeRecord
+# from pydent.aq.models.sample                   import Sample, SampleRecord
+# from pydent.aq.models.upload                   import Upload, UploadRecord
+# from pydent.aq.models.user_budget_association  import UserBudgetAssociation, UserBudgetAssociationRecord
+# from pydent.aq.models.user                     import User, UserRecord
+# from pydent.aq.models.wire                     import Wire, WireRecord
---
+++ pydent/aq/pillow_models.py
@@ -0,0 +1,1 @@
+from pillowtalk import *
