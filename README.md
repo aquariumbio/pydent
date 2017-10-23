b70cddb8131f77a44164705e21351518150930b6
jvrana <justin.vrana@gmail.com>
2017-10-23 15:27:01 -0700

    setup.py working

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+[![travis build](https://img.shields.io/travis/USER/REPO.svg)](https://travis-ci.org/USER/REPO)
+[![Coverage Status](https://coveralls.io/repos/github/USER/REPO/badge.svg?branch=master)](https://coveralls.io/github/USER/REPO?branch=master)
+[![PyPI version](https://badge.fury.io/py/REPO.svg)](https://badge.fury.io/py/REPO)
+
+![module_icon](images/module_icon.png?raw=true)
+
+#### Build/Coverage Status
+Branch | Build | Coverage
+:---: | :---: | :---:
+**master** | [![travis build](https://img.shields.io/travis/USER/REPO/master.svg)](https://travis-ci.org/USER/REPO/master) | [![Coverage Status](https://coveralls.io/repos/github/USER/REPO/badge.svg?branch=master)](https://coveralls.io/github/USER/REPO?branch=master)
+**development** | [![travis build](https://img.shields.io/travis/USER/REPO/development.svg)](https://travis-ci.org/USER/REPO/development) | [![Coverage Status](https://coveralls.io/repos/github/USER/REPO/badge.svg?branch=development)](https://coveralls.io/github/USER/REPO?branch=development)
+
+
+
 Trident
 ===
 The API to Aquarium
---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+import re
+from distutils.core import setup
+
+# about
+__author__ = ''
+__license__ = '?'
+__package__ = "pydent"
+__readme__ = "README"
+
+tests_require = [
+    'pytest',
+    'pytest-runner',
+    'python-coveralls',
+    'pytest-pep8'
+]
+
+install_requires = [
+    # package requirements
+]
+
+classifiers = [],
+
+# setup functions
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/__init__.py').read())
+    if result:
+        return result.group(1)
+    else:
+        raise RuntimeError("Unable to find property {0} in project \"{1}\".".format(prop, project))
+
+def get_version():
+    try:
+        return get_property("__version__", __package__)
+    except RuntimeError as e:
+        raise RuntimeError("Unable to find __version__ string in project \"{0}\"".format(__package__))
+
+# setup
+setup(
+        name=__package__,
+        version=get_version(),
+        packages=["pydent", "pydent/aq"],
+        url='https://github.com/klavinslab/trident',
+        license=__license__,
+        author=__author__,
+        author_email='',
+        keywords='',
+        description='',
+        long_description=read(__readme__),
+        install_requires=install_requires,
+        python_requires='>=3.4',
+        tests_require=tests_require,
+)
