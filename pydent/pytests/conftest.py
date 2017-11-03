5505a2bb387829ef8cc18e081817d6bcef335077
Ben Keller <bjkeller@uw.edu>
2017-11-03 16:14:36 -0700

    Add comments to conftest

--- pydent/pytests/conftest.py
+++ pydent/pytests/conftest.py
@@ -6,17 +6,25 @@ from pydent.aq import AqSession
 
 @pytest.fixture(scope="module")
 def config_path():
+    """
+    Returns the path to the configuration file.
+    """
     folder = os.path.dirname(os.path.abspath(__file__))
     rel_loc = "secrets/config.json"
     return os.path.join(folder, rel_loc)
 
 @pytest.fixture(scope="module")
 def config():
-    with open(os.path.abspath(config_path())) as f:
-        return json.load(f)
+    """
+    Returns the configuration file contents.
+    """
+    with open(os.path.abspath(config_path())) as config_file:
+        return json.load(config_file)
 
 @pytest.fixture(scope="module")
 def load_session():
+    """
+    Creates a session from the configuration.
+    """
     AqSession().create_from_config_file(config_path())
     return 5
-
