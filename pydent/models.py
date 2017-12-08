ee4850ece25c77440e56e31d46a2119299379d08
jvrana <justin.vrana@gmail.com>
2017-12-07 16:05:10 -0800

    update to Code

--- pydent/models.py
+++ pydent/models.py
@@ -100,7 +100,7 @@ class HasCode(object):
         codes = [c for c in self.codes if c.name == name]
         codes = [c for c in codes if not hasattr(
             c, "child_id") or c.child_id is None]
-        if len(codes) > 1:
+        if len(codes) > 0:
             return codes[-1]
 
 
