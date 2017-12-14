dd64bdd07caa57764e26a8481ef78d741a225793
Ben Keller <bjkeller@uw.edu>
2017-12-14 12:27:04 -0800

    Update scheme and index

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -102,16 +102,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinxdoc'
 # html_theme = 'sphinx_rtd_theme'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {
-    'page_width': 'auto'
-}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
