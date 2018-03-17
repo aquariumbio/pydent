770b76cd835152d097c30459ee2ec4a09dd366ba
jvrana <justin.vrana@gmail.com>
2018-03-16 23:09:04 -0700

    version bump to 0.0.8
    accounts for previous pull requests and commits. This bumps the version from 0.0.2 to 0.0.8

--- pydent/__version__.py
+++ pydent/__version__.py
@@ -3,5 +3,5 @@
 __title__ = 'Trident'
 __description__ = 'Aquarium scripting'
 __url__ = 'https://github.com/klavinslab/trident'
-__version__ = '0.0.2'
+__version__ = '0.0.8'
 __author__ = 'Justin Vrana, Eric Klavins, Ben Keller'
