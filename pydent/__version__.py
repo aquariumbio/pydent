0345fae310152af5e92de0de0ac0351934217c13
Justin Vrana <jvrana@uw.edu>
2018-06-28 09:50:21 -0500

    bump to 0.0.15

--- pydent/__version__.py
+++ pydent/__version__.py
@@ -3,5 +3,5 @@
 __title__ = 'Trident'
 __description__ = 'Aquarium scripting'
 __url__ = 'https://github.com/klavinslab/trident'
-__version__ = '0.0.14'
+__version__ = '0.0.15'
 __author__ = 'Justin Vrana, Eric Klavins, Ben Keller'
