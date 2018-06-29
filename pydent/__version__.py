9cc37ebbef7a7d79b4f78932469cc4d147fe444c
Justin Vrana <jvrana@uw.edu>
2018-06-29 14:53:41 -0700

    trigger 0.0.18

--- pydent/__version__.py
+++ pydent/__version__.py
@@ -3,6 +3,6 @@
 __title__ = 'Trident'
 __description__ = 'Aquarium scripting'
 __url__ = 'https://klavinslab.org/trident'
-__version__ = '0.0.17'
+__version__ = '0.0.18'
 __author__ = 'Justin Vrana, Eric Klavins, Ben Keller'
 __author_email__ = "jvrana@uw.edu"
