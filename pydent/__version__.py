3f2c83f61093e2fdbdc8815fc224d09714acafb7
Justin Vrana <jvrana@uw.edu>
2018-06-29 18:41:51 -0700

    version bump to 0.0.25
    this version:
    * fixes a JSON marshalling bug
    * removes prompt-toolkit dependency

--- pydent/__version__.py
+++ pydent/__version__.py
@@ -3,6 +3,6 @@
 __title__ = 'Trident'
 __description__ = 'Aquarium scripting'
 __url__ = 'http://klavinslab.org/trident'
-__version__ = '0.0.24'
+__version__ = '0.0.25'
 __author__ = 'Justin Vrana, Eric Klavins, Ben Keller'
 __author_email__ = "jvrana@uw.edu"
