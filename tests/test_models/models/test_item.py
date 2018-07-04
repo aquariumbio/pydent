7979a3cade11d1acb358a63825098ab8413d5dc4
jvrana <justin.vrana@gmail.com>
2018-07-04 10:51:33 -0700

    update tests for data associations

--- tests/test_models/models/test_item.py
+++ tests/test_models/models/test_item.py
@@ -1,7 +1,45 @@
+from pydent.models import Item
 
+def test_item_get_data_association():
+    item = Item.load(
+        {'created_at': '2018-07-03T13:06:35-07:00',
+         'data': '{"matrix": [[23853, 23853, 23853, 23846, 23846, 23846], [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1]]}',
+         'data_associations': [{'id': 199866,
+                                'key': 'key0',
+                                'object': '{"key0": "hello"}',
+                                'parent_class': 'Item',
+                                'parent_id': 141572,
+                                'rid': 3,
+                                'updated_at': '2018-07-03T14:40:38-07:00',
+                                'upload_id': None},
+                               {
+                                   'id': 1235,
+                                   'key': 'key1',
+                                   'object': '{"key1": [1,2,3,4]}',
+                                   'parent_class': 'Item',
+                                   'parent_id': 141572,
+                                   'rid': 3,
+                                   'updated_at': '2018-07-03T14:40:38-07:00',
+                                   'upload_id': None},
+                               {
+                                   'id': 12356,
+                                   'key': 'key1',
+                                   'object': '{"key1": [2,2,3,4]}',
+                                   'parent_class': 'Item',
+                                   'parent_id': 141572,
+                                   'rid': 3,
+                                   'updated_at': '2018-07-03T14:40:38-07:00',
+                                   'upload_id': None},
+                               ],
+         'id': 141572,
+         'inuse': -1,
+         'location': 'deleted',
+         'locator_id': None,
+         'object_type_id': 789,
+         'quantity': -1,
+         'rid': 1,
+         'sample_id': None,
+         'updated_at': '2018-07-03T14:40:38-07:00'})
 
-# def test_item_data(session):
-#    item = session.Item.find(114553)
-#    assert item, "Item 114553 not found"
-#    data = item.data
-#    print(type(data))
+    assert item.get('key0') == 'hello'
+    assert item.get('key1') == [[1,2,3,4], [2,2,3,4]]
