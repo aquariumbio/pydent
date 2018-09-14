7e3a5c2023154c0c11cace2e39cb6df3c9572abc
Justin Vrana <jvrana@uw.edu>
2018-09-14 15:27:34 -0700

    exceptions update

--- pydent/exceptions.py
+++ pydent/exceptions.py
@@ -5,12 +5,11 @@ Trident exceptions
 from requests.exceptions import RequestException, ConnectTimeout
 
 
-class TridentRequestError(RequestException):
+class TridentRequestError(IOError):
     """There was an ambiguous exception that occured handling your request."""
-    def __init__(self, message, response):
-        self.message = message
-        self.response = response
 
+class AquariumError(IOError):
+    """Aquarium raised an error"""
 
 class TridentJSONDataIncomplete(RequestException):
     """JSON data contains a null value and may be incomplete."""
