92f5d8a98d8909fe078fceb3ae9207eb7baf5511
Ben Keller <bjkeller@uw.edu>
2018-09-21 07:09:31 -0700

    Add tests for collections dimensions and matrix methods

--- tests/archived/test_collection.py
+++ tests/archived/test_collection.py
@@ -15,19 +15,22 @@ def example_collection(session, scope='module'):
     return collection
 
 
+@pytest.fixture
+def ex_part_associations(session, example_collection, scope='module'):
+    collection = example_collection
+    part_associations = session.PartAssociation.where(
+        {'collection_id': collection.id}
+    )
+    assert part_associations
+    assert len(part_associations) > 0
+    return part_associations
+
+
 class TestCollection:
 
-    def get_part_associations(self, session, collection):
-        part_associations = session.PartAssociation.where(
-            {'collection_id': collection.id}
-        )
-        assert part_associations
-        assert len(part_associations) > 0
-        return part_associations
-
-    def test_parts(self, session, example_collection):
+    def test_parts(self, session, example_collection, ex_part_associations):
         collection = example_collection
-        part_associations = self.get_part_associations(session, collection)
+        part_associations = ex_part_associations
 
         expected_part = next(iter([
             assoc.part for assoc in part_associations
@@ -37,19 +40,30 @@ class TestCollection:
         actual_part = collection.part(0, 0)
         assert expected_part.id == actual_part.id
 
-    """def test_matrix(self, session, example_collection):
+    def test_dimensions(self, session, example_collection):
         collection = example_collection
-        part_associations = self.get_part_associations(session, collection)
+        assert collection.dimensions
+        row, col = collection.dimensions
+        assert row == 8 and col == 12
 
-        matrix = list()
-        for row in range(8):
-            row_assoc = [assoc for assoc in part_associations
-                         if assoc.row == row]
+    def test_matrix(self, session, example_collection, ex_part_associations):
+        collection = example_collection
+        part_associations = ex_part_associations
+        num_row, num_col = collection.dimensions
+
+        expected_matrix = list()
+        for row in range(num_row):
+            row_assoc = [
+                assoc for assoc in part_associations if assoc.row == row]
             row_list = list()
-            for col in range(12):
-                if
+            for col in range(num_col):
+                sample_id = None
+                col_assoc = [
+                    assoc for assoc in row_assoc if assoc.column == col]
+                if col_assoc:
+                    assoc = next(iter(col_assoc))
+                    sample_id = assoc.part.sample.id
+                row_list.append(sample_id)
+            expected_matrix.append(row_list)
 
-            matrix.append(row_list)
-
-        print(json.dumps(matrix, indent=2))
-        assert False"""
+        assert collection.matrix == expected_matrix
