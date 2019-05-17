cf76bd530793b9d96e4bfc7d786915a736098917
Justin Vrana <justin.vrana@gmail.com>
2019-05-17 12:50:03 -0700

    jupyter async bug
    Recent changes to jupyter notebook caused asynch operations to fail. This patch fixes issues with running trident in a juypyter notebook. I'm unsure how to cature future jupyter notebooks in an automated way however.
    
    
    Former-commit-id: a957ea504dacb71601fd9895d05cf023a1c1459c

--- pydent/__init__.py
+++ pydent/__init__.py
@@ -22,6 +22,10 @@ Submodules
 
 """
 
+# without nest_asyncio, Jupyter notebooks will crash if an async method is used
+import nest_asyncio
+nest_asyncio.apply()
+
 from .__version__ import __description__, __author__, __version__, __url__, __title__, __pypi__
 from .aqsession import AqSession
 from .base import ModelBase, ModelRegistry
