c83b4ffc4b6e2bd35ff8358c13298d191c74ec3e
Justin Vrana <justin.vrana@gmail.com>
2019-05-20 20:30:41 -0700

    Update __version__.py
    Former-commit-id: 6c537d46c6184344c801e7bb394219dedadee994

--- pydent/__version__.py
+++ pydent/__version__.py
@@ -3,7 +3,7 @@
 __title__ = 'Trident'
 __description__ = 'Aquarium scripting'
 __url__ = 'http://klavinslab.org/trident'
-__version__ = '1.0.0a1'
+__version__ = '1.0.0a2'
 __author__ = 'Justin Vrana, Eric Klavins, Ben Keller'
 __author_email__ = "jvrana@uw.edu"
 __pypi__ = "https://pypi.org/project/pydent/"
