b9728d6f1f07be5252fc9e256a1d0045e8e1b36c
jvrana <justin.vrana@gmail.com>
2019-06-12 07:49:25 -0700

    update version handling
    Former-commit-id: 4f8fde8057c953c96c282ca6b35842a0f6ddba18

--- pydent/__version__.py
+++ pydent/__version__.py
@@ -1,9 +1,11 @@
 # TRIDENT
+import configparser
+from os.path import join, abspath, dirname
 
-__title__ = 'Trident'
-__description__ = 'Aquarium scripting'
-__url__ = 'http://klavinslab.org/trident'
-__version__ = '0.1.0a3'
-__author__ = 'Justin Vrana, Eric Klavins, Ben Keller'
-__author_email__ = "jvrana@uw.edu"
-__pypi__ = "https://pypi.org/project/pydent/"
+here = dirname(abspath(__file__))
+
+
+config = configparser.ConfigParser()
+config.read(join(here, '..', 'pyproject.toml'))
+
+info = config['tool.poetry']
