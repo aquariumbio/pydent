d85a59968c1d4233194aea11d2fe9ed8fe61d1ba
jvrana <justin.vrana@gmail.com>
2019-06-13 15:28:21 -0700

    Create test_browser_loads_recursively.py

---
+++ tests/tests_with_server_and_cached_results/test_browser/test_browser_loads_recursively.py
@@ -0,0 +1,14 @@
+from pydent.browser import Browser
+
+
+def test_browser_loads_wires(session):
+    """
+    The Plan class has a special query hook that automatically grabs wires as well.
+    By default, the update cache will recursively update models that have been deserialized from
+    a model list. We expect that the 'Wire' model is in the model cache when we load a plan.
+    """
+
+    browser = Browser(session)
+    assert "Wire" not in browser.model_cache
+    browser.one("Plan")
+    assert "Wire" in browser.model_cache
