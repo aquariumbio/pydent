8b82938dc68edbf1e0aab327001df1d0f2bdc836
jvrana <justin.vrana@gmail.com>
2019-06-19 13:53:00 -0700

    remove depreciated examples

--- examples/login.py
+++
@@ -1,5 +0,0 @@
-from pydent import AqSession
-
-# basic login
-nursery = AqSession("username", "password", "www.aquarium_nursery.url")
-production = AqSession("username", "password", "www.aquarium_production.url")
