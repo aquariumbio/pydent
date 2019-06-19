71c144fd1b017baf4067e1013189ba770efc60cc
jvrana <justin.vrana@gmail.com>
2019-06-19 16:38:12 -0700

    staging for release v0.1.0

--- tests/tests_with_server_and_cached_results/model_tests_with_server/test_inventory_on_server/test_retrieve_upload.py
+++
@@ -1,7 +0,0 @@
-class TestUpload:
-    def test_retrieve_upload(self, session):
-        upload = session.Upload.find(10502)
-        assert upload
-        assert upload.size == 3011
-        assert upload.name == 'jid_57977_item_120721_03132018_cal_gfp.csv'
-        assert upload.job.id == 94686
