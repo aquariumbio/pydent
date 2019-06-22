856f70d8530ea0fc5098d1bd73ebd53ffd343542
jvrana <justin.vrana@gmail.com>
2019-06-22 08:53:25 -0700

    live demos

---
+++ demo/doitlive/intro.sh
@@ -0,0 +1,22 @@
+# Recorded with the doitlive recorder
+#doitlive shell: /bin/bash
+#doitlive prompt: default
+#doitlive speed: 3
+
+```python
+import pydent
+session = pydent.login('neptune', 'http://0.0.0.0', 'aquarium')
+session
+s = session.Sample.one()
+print(s)
+print(s.properties)
+primertype = session.SampleType.find_by_name("Primer")
+newprimer = session.Sample.new(name="MyNewPrimer", project="Demo", sample_type_id=primertype.id)
+newprimer.field_values = []
+newprimer.update_properties({'Anneal Sequence': 'AGGGTTCTGGGTTGTGCTGTA'})
+newprimer.save()
+print(newprimer.id)
+exit()
+```
+
+
