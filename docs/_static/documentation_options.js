0908a24e7a495a3952e3550d590b542ef9eb01b2
Justin Vrana <justin.vrana@gmail.com>
2019-06-21 21:10:38 -0700

    update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.2-alpha',
+    VERSION: 'v0.1.3-alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
