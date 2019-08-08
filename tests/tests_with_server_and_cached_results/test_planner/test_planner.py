4fd51c35d24e3b1800e1fc722224842441b9e89e
Justin Vrana <justin.vrana@gmail.com>
2019-08-08 14:36:07 -0700

    test for RESTRICT_TO_ONE option for setting items in planner
    added RESTRICT_TO_ONE_ON_SERVER option
    
    
    Former-commit-id: 7fd608a7e13024871f668fe7476f3e2412660650

--- tests/tests_with_server_and_cached_results/test_planner/test_planner.py
+++ tests/tests_with_server_and_cached_results/test_planner/test_planner.py
@@ -69,3 +69,38 @@ def test_combine_plans(planner_example):
     assert len(combined.plan.wires) == len(
         planner_example.plan.wires
     ), "number of wires should remain the same"
+
+
+# TODO: more tests for setting items
+@pytest.mark.parametrize("restrict_to_one", [True, False])
+class TestSetItem():
+    def test_restrict_to_one(self, session, restrict_to_one):
+        with session.with_cache() as sess:
+            sess.set_verbose(True)
+            p = Planner(sess)
+            for i in range(2):
+
+                comp_cell_type = session.ObjectType.find_by_name("Yeast Competent Cell")
+                comp_cells = session.Item.last(100, query='object_type_id = {} AND location != "deleted"'.format(comp_cell_type.id))
+                grouped_by_samples = {}
+                for c in comp_cells:
+                    grouped_by_samples.setdefault(c.sample_id, list()).append(c)
+                more_than_one = [cells for cells in grouped_by_samples.values() if len(cells) > 1][0]
+                assert more_than_one
+                parent_sample = more_than_one[0].sample
+
+                op = p.create_operation_by_name("Yeast Transformation")
+                p.set_field_value(op.input("Parent"), sample=parent_sample)
+                if restrict_to_one:
+                    p.set_to_available_item(op.input("Parent"), item_preference=p.ITEM_SELECTION_PREFERENCE.RESTRICT_TO_ONE)
+                else:
+                    p.set_to_available_item(op.input("Parent"))
+            items = set()
+            for op in p.plan.operations:
+                for fv in op.field_values:
+                    if fv.child_item_id:
+                        items.add(fv.child_item_id)
+            if restrict_to_one:
+                assert len(items) == 2
+            else:
+                assert len(items) == 1
