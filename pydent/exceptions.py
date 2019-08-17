c2d58524b9930da1b7b66e5b08c1e7bb78d8fbf5
Justin Vrana <justin.vrana@gmail.com>
2019-08-16 18:48:30 -0700

    base exception for all new TridentExceptions
    Former-commit-id: c19a4af789420d92ee5eb8e604f4d901b4f429c6

--- pydent/exceptions.py
+++ pydent/exceptions.py
@@ -4,40 +4,43 @@ Trident exceptions
 
 from requests.exceptions import RequestException, ConnectTimeout
 
+class TridentBaseException(Exception):
+    """Base exception for all trident errors"""
 
-class TridentRequestError(IOError):
+
+class TridentRequestError(TridentBaseException):
     """There was an ambiguous exception that occurred handling your request."""
 
 
-class AquariumError(IOError):
+class AquariumError(TridentBaseException):
     """Aquarium raised an error"""
 
 
-class ForbiddenRequestError(Exception):
+class ForbiddenRequestError(TridentBaseException):
     """Raised when Trident attempts to make a request after requests have been
     explicitly turned off."""
 
 
-class TridentJSONDataIncomplete(RequestException):
+class TridentJSONDataIncomplete(TridentBaseException):
     """JSON data contains a null value and may be incomplete."""
 
 
-class TridentLoginError(RequestException):
+class TridentLoginError(TridentBaseException):
     """Trident is not properly connected to the server.
      Verify login credentials are correct."""
 
 
-class TridentTimeoutError(ConnectTimeout):
+class TridentTimeoutError(TridentBaseException):
     """Trident took too long to respond"""
 
 
-class TridentModelNotFoundError(AttributeError):
+class TridentModelNotFoundError(TridentBaseException):
     """Trident could not find model in list of models."""
 
 
-class AquariumModelError(Exception):
+class AquariumModelError(TridentBaseException):
     """An error occurred with this Aquarium model"""
 
 
-class NoSessionError(Exception):
+class NoSessionError(TridentBaseException):
     """There was no session attached to the model, but one is required."""
